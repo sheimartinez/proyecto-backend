e96198e5acff30d791e501dcd347317feaec70d8
SheilaMart <sheilamartinez372@gmail.com>
2026-01-12 14:56:29 -0300

    usando railway

--- conexion.js
+++ conexion.js
@@ -1,22 +1,23 @@
 const mysql = require('mysql2'); /*importa la librería mysql2*/
 
-const conexion = mysql.createConnection({ /*Crea una conexión individual a la base de datos.*/
+const conexion = mysql.createPool({ /*Crea una conexión individual a la base de datos.*/
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-conexion.connect(error => { /*Intenta inicializar la conexión con la base de datos.*/
-  if (error) {
-    console.error('Error conectando a MySQL:', error); /*Muestra por consola un mensaje de error y el objeto error con detalles (puede incluir código, mensaje, etc.)*/
-    return;
+conexion.getConnection((err, connection) => { /*Intenta inicializar la conexión con la base de datos.*/
+  if (err) {
+    console.error('Error conectando a MySQL:', err); /*Muestra por consola un mensaje de error y el objeto error con detalles (puede incluir código, mensaje, etc.)*/
+  } else {
+    console.log('Conectado a MySQL'); /*Si no hubo error, imprime que la conexión fue exitosa (railway).*/
+    connection.release();
   }
-  console.log('Conectado a MySQL'); /*Si no hubo error, imprime que la conexión fue exitosa (Aiven).*/
 });
 
 module.exports = conexion; /*Hace que la variable conexion pueda ser usada desde otros archivos del proyecto.*/
